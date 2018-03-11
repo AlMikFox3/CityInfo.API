96b4f6b90bf828969689446d7a60b2b525d1b94f
AlMikFox3 <sasmit1997@outlook.com>
2018-03-12 01:28:55 +0530

    Initial Commit

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet( )]
+        public IActionResult GetCities()
+        {
+            return Ok(CityDataStore.Current.Cities);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var CityToReturn = CityDataStore.Current.Cities.FirstOrDefault(k => k.Id == id);
+            if (CityToReturn == null){
+                return NotFound();
+            }
+            return (Ok(CityToReturn));
+        }
+    }
+}
---
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{id}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int id)
+        {
+            var city_ret = CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+            if (city_ret == null)
+            {
+                return NotFound();
+            }
+            return Ok(city_ret.PointsOfInterest);
+   
+        }
+        [HttpGet("{cid}/pointsofinterest/{pid}")]
+        public IActionResult GetPointOfInterest(int cid, int pid)
+        {
+            var city = CityDataStore.Current.Cities.FirstOrDefault(c => c.Id == cid);
+            if (city == null)
+                return NotFound();
+            var PointsOfInt = city.PointsOfInterest.FirstOrDefault(p=> p.Id == pid);
+            if (PointsOfInt == null)
+                return NotFound();
+            return Ok(PointsOfInt);
+        }
+    }
+}
